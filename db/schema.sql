2dc95b190b47ac354a41831994be0048f8c22bad
Samantha Gaiser <sgaiser3@gmail.com>
2020-01-21 21:48:47 -0500

    working on connections, orm, and msyql table.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### 
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE BURGERS
+(
+	id int NOT NULL IDENTITY (1,1),
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+-- vs code throws an error. This is how I've worked around it.
+-- this page explains more.
+-- https://stackoverflow.com/questions/15049434/sql-create-statement-incorrect-syntax-near-auto-increment
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Swiss');
+INSERT INTO burgers (name) VALUES ('Blue Mountain');
+INSERT INTO burgers (name, devoured) VALUES ('Green Leaf', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheese and Guac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheddar Crisp', true);
+INSERT INTO burgers (name) VALUES ('Veggie');

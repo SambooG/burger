de1b7e589d2ab70378a2140942deb3f6060668c8
Samantha Gaiser <sgaiser3@gmail.com>
2020-02-21 17:06:40 -0500

    heroku connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql');
 
-
-const connection = mysql.createConnection({
+const connectionConfig = process.env.JAWSDB_URL
+  || {
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'burgers_db'
-  });
+  }
+
+
+// mysql://d8qu9g04i28b27d1:gjypdznlij41q1tm@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mrre835i5wv22dcz
+const connection = mysql.createConnection(connectionConfig);
+
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
